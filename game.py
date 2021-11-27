e87b36fc8447287da66435c7b5b9fdd4f9a0bf59
forobot-sh <94891606+forobot-sh@users.noreply.github.com>
2021-11-27 12:34:28 +0800

    module bullets
    完成子弹杀死僵尸的功能

--- game.py
+++ game.py
@@ -132,8 +132,12 @@ class MainGame:
     @staticmethod
     def load_bullets():
         for bullet in MainGame.bullets:
-            bullet.move()
-            bullet.display()
+            if bullet.live:
+                bullet.move()
+                bullet.display()
+                bullet.hit_zombie()
+            else:
+                MainGame.bullets.remove(bullet)
 
     @staticmethod
     def produce_zombie():
@@ -218,6 +222,8 @@ class PeaShooterBullet(pygame.sprite.Sprite):
         self.rect.left = pea_shooter.rect.left + 40
         self.rect.top = pea_shooter.rect.top + 20
         self.speed = 10
+        self.damage = 100
+        self.live = True
 
     def move(self):
         if self.rect.left < SCREEN_WIDTH:
@@ -226,6 +232,15 @@ class PeaShooterBullet(pygame.sprite.Sprite):
             # 子弹移出列表
             MainGame.bullets.remove(self)
             pass
+
+    def hit_zombie(self):
+        for zombie in MainGame.zombie_list:
+            if pygame.sprite.collide_rect(self, zombie):
+                zombie.hp -= self.damage
+                self.live = False
+                if zombie.hp <= 0:
+                    zombie.live = False
+                    pass
 
     def display(self):
         MainGame.screen.blit(self.image, self.rect)
@@ -271,6 +286,7 @@ class Zombie(pygame.sprite.Sprite):
             m = MainGame.maps_list[top - 1][left]
             m.can_grow = True
             MainGame.plant_list.remove(plant)
+
 
 if __name__ == '__main__':
     game = MainGame()
